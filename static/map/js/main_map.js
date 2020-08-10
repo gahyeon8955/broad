0218dfbbf258f67cf8ea402cdd2c5b61be964cc4
eunchae619 <eunchae619@naver.com>
2020-08-10 13:43:03 +0900

    지역선택시, 중심위치 변경되는것까지 구현

--- static/map/js/main_map.js
+++ static/map/js/main_map.js
@@ -1,8 +1,42 @@
-var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-var options = {
+const container = document.getElementById("map"); //지도를 담을 영역의 DOM
+const mapInit = document.querySelector(".jsMapInit"); //첫 지도이미지 DOM
+const regionSelectBox = document.querySelector(".jsRegionSelectBox"); //지역선택박스 DOM
+const regionSelect = document.querySelector(".jsRegionSelect"); //지역선택 Select태그 DOM
+var map;
+
+let options = {
   //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
+  center: new kakao.maps.LatLng(35.205392, 128.1267993), //지도의 중심좌표.
+  level: 10, //지도의 레벨(확대, 축소 정도)
 };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+// 제일처음 시작되는 함수로, map_init에서 지역이 클릭되면 카카오맵이 호출됨
+const startMap = () => {
+  mapInit.classList.add("set_none"); //첫 지도이미지 display:none 추가
+  regionSelectBox.classList.replace("set_none", "set_block"); //지역선택박스 none -> block으로 변경
+  regionSelectBox.classList.add("set_z-index_6"); //지역선택박스 Kakao map 위로 오게 설정
+  map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+};
+
+// 지역별 중심좌표를 모아놓은 객체
+const regionCoordinate = {
+  진주시: [35.205392, 128.1267993],
+  부산광역시: [35.1593242, 128.9931503],
+};
+
+// 중심좌표를 변경하는 함수
+function setCenter(lat, lng) {
+  // 이동할 위도 경도 위치를 생성합니다
+  var moveLatLon = new kakao.maps.LatLng(lat, lng);
+  // 지도 중심을 이동 시킵니다
+  map.setCenter(moveLatLon);
+}
+
+// select박스에서 지역이 변경될시 호출되는 함수
+const changeRegion = () => {
+  let regionSelectedValue = regionSelect.value;
+  setCenter(
+    regionCoordinate[regionSelectedValue][0],
+    regionCoordinate[regionSelectedValue][1]
+  );
+};
