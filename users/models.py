25cf47ba950724b60dd7385228697b9785d354c7
성은수 <qobac9@likelion.org>
2020-08-19 13:54:34 +0900

    users models.py 수정

--- users/models.py
+++ users/models.py
@@ -6,4 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField()
+    password = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=20, blank = True)
+    
+    def __str__(self):
+            return self.name
