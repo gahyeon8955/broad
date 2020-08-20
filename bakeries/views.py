8f261b8aaea2292210e3f34c903ca83105418302
eunchae619 <eunchae619@naver.com>
2020-08-20 16:13:00 +0900

    리뷰작성 view 구현

--- bakeries/views.py
+++ bakeries/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import WriteReviewForm
+from . import models as bakery_models
+
 
 # Create your views here.
 
@@ -27,5 +30,16 @@ def user_review_list(request):
     return render(request, "bakeries/user_review_list.html")
 
 
-def review_write(request):
-    return render(request, "bakeries/review_write.html")
+# 현재는 테스트용으로 bakery_id=4로 기본값4 줬지만, 다른것들과 연동되면 '=4'부분 제거 예정
+def review_write(request, bakery_id=4):
+    if request.method == "POST":
+        form = WriteReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.bakery = bakery_models.Bakery.objects.get(id=bakery_id)
+            review.user = request.user
+            review.save()
+            return redirect("bakeries:user_review_list")
+    else:
+        form = WriteReviewForm()
+    return render(request, "bakeries/review_write.html", {"form": form})
