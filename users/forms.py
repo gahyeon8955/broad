fc6e122b4bc0b0962ed2fffbbf1370f192514484
eunchae619 <eunchae619@naver.com>
2020-08-22 22:24:49 +0900

    로그인,회원가입 오류수정

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,21 @@
 from django import forms
 from .models import User
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['email','username','password']
-    
+        model = get_user_model()
+        fields = ["email", "username", "password"]
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={}))
 
     def clean(self):
-        username = self.cleaned_data.get('username')
-        password = self.cleaned_data.get('password')
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
         try:
             user = User.objects.get(username=username)
             if user.check_password(password):
@@ -24,19 +25,15 @@ class LoginForm(forms.Form):
         except User.DoesNotExist:
             self.add_error("username", forms.ValidationError("계정이 존재하지 않습니다"))
 
+
 class SignUpForm(forms.ModelForm):
-    
     class Meta:
-        model = User
-        fields = ['username','email']
+        model = get_user_model()
+        fields = ["username", "email"]
 
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={})
-    )
+    password = forms.CharField(widget=forms.PasswordInput(attrs={}))
 
-    confirm_password = forms.CharField(
-        widget=forms.PasswordInput(attrs={})
-    )
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={}))
 
     def clean_confirm_password(self):
         password = self.cleaned_data.get("password")
@@ -44,4 +41,5 @@ class SignUpForm(forms.ModelForm):
         if password != confirm_password:
             raise forms.ValidationError("비밀번호가 일치하지 않습니다")
         else:
-            return password            
+            return password
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from .forms import UpdateProfileForm
-from django.contrib.auth import authenticate, login as auth_login,logout as auth_logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from .forms import LoginForm
 from .forms import SignUpForm
 
@@ -13,35 +13,38 @@ def index(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            auth_user = authenticate(request,username=username,password=password)
+            auth_user = authenticate(request, username=username, password=password)
             if auth_user is not None:
-                auth_login(request,auth_user)
-                return redirect('users/login.html')
+                auth_login(request, auth_user)
+                return redirect("map:main_map")
     else:
         form = LoginForm()
-    return render(request, "users/login.html",{"form":form})
+    return render(request, "users/login.html", {"form": form})
 
 
 def signup(request):
-    if request.method == 'POST':
-        form = forms.SignUpForm(request.POST)
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
             username = form.cleaned_data.get("username")
             email = form.cleaned_data.get("email")
             password = form.cleaned_data.get("password")
-            user = User.objects.create_user(username=username,email=email,password=password)
-            auth_user = authenticate(request,username=username,password=password)
+            user = User.objects.create_user(
+                username=username, email=email, password=password
+            )
+            auth_user = authenticate(request, username=username, password=password)
             if auth_user is not None:
-                auth_login(request,auth_user)
-                return redirect('blog:index')
+                auth_login(request, auth_user)
+                return redirect("map:main_map")
     else:
-        form = SignUpForm()    
-    return render(request, "users/signup.html",{'form':form})
+        form = SignUpForm()
+    return render(request, "users/signup.html", {"form": form})
 
 
 def profile_view(request):
@@ -54,7 +57,7 @@ def profile_update(request):
         form = UpdateProfileForm(request.POST, instance=user)
         if form.is_valid():
             user = form.save()
-            return redirect('users:profile_update')
+            return redirect("users:profile")
     else:
         form = UpdateProfileForm(instance=user)
-    return render(request, "users/profile_update.html")
+    return render(request, "users/profile_update.html", {"form": form})
