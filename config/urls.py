6e90ac7be208430c6bfbdf9976e90be8c3bb595d
eunchae619 <eunchae619@naver.com>
2020-08-29 16:06:07 +0900

    로컬셋팅

--- config/urls.py
+++ config/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path("bakery/", include("bakeries.urls", namespace="bakeries")),
     path("post/", include("posts.urls", namespace="posts")),
     path("", include("users.urls", namespace="users")),
-    # url(r"media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT,}),
+    url(r"media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT,}),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
